913f5c02553f15cd438b00422ee8329fb1eea09e
Adam Ingor <adamingor@192.168.100.4>
2025-07-21 14:45:51 +0300

    Add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/shared/provider";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const nunito = Nunito({
   subsets: ["latin", "cyrillic"],
